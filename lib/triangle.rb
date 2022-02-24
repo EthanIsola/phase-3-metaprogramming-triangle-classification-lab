9ccc8085f86ee11ec6bdbb85dda47c1f4f5d52db
Ethan Isola <isolaethan@gmail.com>
2022-02-24 15:37:37 -0600

    commit tree 52ec52789e45a9d3bcab994201ccbe5e31f3ee3e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
+  def kind
+    if(side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+      begin
+        raise TriangleError
+      end
+    elsif(side1 && side2 && side3) > 0
+      if (side1 === side2) && (side1 == side3)
+        :equilateral
+      elsif ((side1 === side2) && (side1 != side3)) || ((side2 === side3) && (side2 != side1)) ||((side1 === side3) && (side2 != side1))
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
 end
